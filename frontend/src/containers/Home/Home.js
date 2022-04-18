e791c4e03c4a0788b2d1f0f02cd0a5f104587c76
F-KZ <supreme-soul@hotmail.fr>
2022-04-18 11:47:36 +0200

    url 404 a corriger

---
+++ frontend/src/containers/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Static Images
+import logo from "../../images/logo.png";
+
+// Styles
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="background_image">
+            <img src={logo} className="logo" alt="Logo de Groupomania, entreprise de grande distribution européenne" />
+            <div className="welcome">
+                <h3 className="title">Bienvenue</h3>
+                <p className="message">sur Groupomania</p> 
+            </div>
+        </div>
+    );
+};
+
+export default Home;
