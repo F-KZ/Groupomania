7fe5795ae8a8b31b64e8a174b729e1ed62ed4a84
F-KZ <supreme-soul@hotmail.fr>
2024-02-05 13:52:15 +0100

    A

--- backend/app.js
+++ backend/app.js
@@ -36,6 +36,7 @@ app.use(function (req, res, next) {
 
 app.use(helmet());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.use("/", (req,res) => {
   res.send("ça marche")
